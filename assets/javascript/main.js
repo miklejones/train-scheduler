3e8c5419fc9e5b084947abcedd8ba231dfd84b0d
Michael Garland <mgarland@pmgnet.local>
2018-03-27 16:07:17 -0400

    visual is there, connecting to firebase and moment.js

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC77j2--hKP7xHVKn2hldSp8wjgommYGJE",
+    authDomain: "train-scheduler-2a9e9.firebaseapp.com",
+    databaseURL: "https://train-scheduler-2a9e9.firebaseio.com",
+    projectId: "train-scheduler-2a9e9",
+    storageBucket: "",
+    messagingSenderId: "155200905163"
+};
+firebase.initializeApp(config);
