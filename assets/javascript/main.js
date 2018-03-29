3897936383c881badc3bafc6271339141db8a26e
Michael Garland <michaelcgarland@gmail.com>
2018-03-29 15:28:02 -0400

    firebase working, time for some logic

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,3 +8,28 @@ var config = {
     messagingSenderId: "155200905163"
 };
 firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
+// Initial values
+var trainName = "";
+var destination = "";
+var firstTrainTime = 0;
+var frequency = 0;
+
+$('.btn-primary').on('click', function(event) {
+
+    //Capture user inputs and store then into variables
+    trainName = $('#trainName').val().trim();
+    destination = $('#destination').val().trim();
+    firsttrainTime = $('#firstTrainTime').val().trim();
+    frequency = $('#frequency').val().trim();
+
+    database.ref().push({
+        trainName: 'sdf',
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency
+    })
+})
